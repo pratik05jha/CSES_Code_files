2aa78ff39890986ac432a14d684c438006ebea80
Pratik Kumar Jha <pratik.22bce7511@vitapstudent.ac.in>
2025-12-26 01:07:34 +0530

    missing no done

--- missingNumber.java
+++ missingNumber.java
@@ -1,2 +1,18 @@
+import java.util.*;
+
 public class missingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+
+        long n = sc.nextLong();
+        long expectedSum = (n * (n + 1)) / 2;
+        long actualSum = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            actualSum += sc.nextInt();
+        }
+
+        System.out.println(expectedSum - actualSum);
+    }
 }
